361b34b76f27ef972d965fb3d5048befea0e92d2
Bruno <brunoalvimduarte@gmail.com>
2023-01-11 17:09:46 -0300

    criando a página do carrinho de compras

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route path="/shopping-cart" component={ ShoppingCart } />
       </Switch>
     </div>
   );
