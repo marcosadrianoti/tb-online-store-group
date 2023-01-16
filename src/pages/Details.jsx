f4bb5de4c6e8355257573d666ba8438d095b3748
Filipe Bueno <filipe_bueno85@live.com>
2023-01-16 16:05:43 -0300

    Avalie e comente acerca de um produto em sua tela de exibição detalhada

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,24 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-
 import { Link } from 'react-router-dom';
+import Error from '../components/Error';
 import { getProductById } from '../services/api';
+
+const one = 1;
+const two = 2;
+const three = 3;
+const four = 4;
+const five = 5;
 
 class Details extends Component {
   state = {
     product: [],
+    ratingIndex: [one, two, three, four, five],
+    email: '',
+    rating: '',
+    text: '',
+    isValid: true,
+    // isDisabled: true,
   };
 
   componentDidMount() {
@@ -37,8 +49,59 @@ class Details extends Component {
     }
   };
 
+  validate = () => {
+    const { email, rating } = this.state;
+    const emailRegex = /^[a-z0-9.-_]+@[a-z0-9]+\.[a-z]+\)?$/i.test(email);
+    if (email.length !== 0
+      && emailRegex && rating.length !== 0) {
+      this.addRating();
+      this.setState({
+        isValid: true,
+        email: '',
+        text: '',
+        rating: '',
+      });
+      return;
+    }
+    this.setState({
+      isValid: false,
+    });
+  };
+
+  addRating = () => {
+    const { match: { params: { id } } } = this.props;
+    const { email, rating, text } = this.state;
+    const evaluator = localStorage.getItem(id);
+    if (evaluator) {
+      const newRatings = {
+        email,
+        text,
+        rating,
+      };
+      const ratings = JSON.parse(evaluator);
+      localStorage.setItem(id, JSON.stringify([...ratings, newRatings]));
+    } else {
+      const newRatings = {
+        email,
+        text,
+        rating,
+      };
+      localStorage.setItem(id, JSON.stringify([newRatings]));
+    }
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
-    const { product } = this.state;
+    const { product, ratingIndex, email, text, isValid } = this.state;
+    const { match: { params: { id } } } = this.props;
+    const getRatings = localStorage.getItem(id);
+    const ratings = JSON.parse(getRatings);
     return (
       <div>
         <h3 data-testid="product-detail-name">{product.title}</h3>
@@ -61,7 +124,59 @@ class Details extends Component {
             Carrinho de Compras
           </Link>
         </button>
-
+        <div>
+          <fieldset>
+            <form>
+              <input
+                data-testid="product-detail-email"
+                type="email"
+                name="email"
+                value={ email }
+                id="email"
+                placeholder="e-mail"
+                onChange={ this.handleChange }
+              />
+              <div>
+                {
+                  ratingIndex.map((index) => (
+                    <input
+                      key={ index }
+                      type="radio"
+                      value={ index }
+                      data-testid={ `${index}-rating` }
+                      name="rating"
+                      onChange={ this.handleChange }
+                    />))
+                }
+              </div>
+              <textarea
+                data-testid="product-detail-evaluation"
+                name="text"
+                value={ text }
+                placeholder="digite seu comentário"
+                onChange={ this.handleChange }
+              />
+              <button
+                type="button"
+                data-testid="submit-review-btn"
+                onClick={ () => this.validate(id) }
+              >
+                Avaliar Produto
+              </button>
+            </form>
+            {!isValid
+            && (<Error />)}
+          </fieldset>
+          <fieldset>
+            {ratings && ratings.map((rating, index) => (
+              <div key={ index }>
+                <p data-testid="review-card-email">{ rating.email }</p>
+                <p data-testid="review-card-rating">{ rating.rating }</p>
+                <p data-testid="review-card-evaluation">{ rating.text }</p>
+              </div>
+            ))}
+          </fieldset>
+        </div>
       </div>
     );
   }
